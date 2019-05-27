829906e4f92c6be240e65ad93628a0b0c3fa80ce
Alex <ale8under8@gmail.com>
2019-05-27 16:30:00 -0700

    Updates

---
+++ precache-manifest.ef3d6ad35ee4662f4cf906668af3e409.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69ecda25881826061971",
+    "url": "/Drum-Machine/static/js/runtime~main.2d2b62a3.js"
+  },
+  {
+    "revision": "f402cf4627bbeb21bbe3",
+    "url": "/Drum-Machine/static/js/main.945150c8.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/Drum-Machine/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "f402cf4627bbeb21bbe3",
+    "url": "/Drum-Machine/static/css/main.6f5e50f6.chunk.css"
+  },
+  {
+    "revision": "8b78bfb7dad25642fa0e01c6673a3aef",
+    "url": "/Drum-Machine/index.html"
+  }
+];
